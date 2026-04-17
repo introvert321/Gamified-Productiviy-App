daec00c79c9b1fa6e876cb293299106efdf1393a
Deepanshu <deepanshu@Deepanshus-MacBook-Air.local>
2026-04-17 23:06:47 +0530

    first commit

---
+++ gamified-productivity/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Let's begin working on our first dream project
+    </div>
+  );
+}
+
+export default App;
